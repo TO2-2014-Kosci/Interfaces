f9cac769ad113464961a1628d2bd690a7232e343
jlabuz <vaduz16@gmail.com>
2014-11-25 11:15:37 +0100

    bugfix

--- src/to2/dice/controllers/GameControllerFactory.java
+++ src/to2/dice/controllers/GameControllerFactory.java
@@ -7,18 +7,18 @@ import to2.dice.controllers.ngames.PlusCountingStrategy;
 import to2.dice.server.GameServer;
 
 public class GameControllerFactory {
-    public GameController createGameControler(GameServer server, GameSettings settings){
+    public GameController createGameControler(GameServer server, GameSettings settings, String creator){
         GameController gameController = null;
         GameType type = settings.getGameType();
         switch (type){
             case NPLUS:
-                gameController = new NGameController(server, settings, new PlusCountingStrategy());
+                gameController = new NGameController(server, settings, creator, new PlusCountingStrategy());
                 break;
             case NMUL:
-                gameController = new NGameController(server, settings, new MulCountingStrategy());
+                gameController = new NGameController(server, settings, creator, new MulCountingStrategy());
                 break;
             case POKER:
-                gameController = new PokerGameController(server, settings);
+                gameController = new PokerGameController(server, settings, creator);
                 break;
             default:
                 //THROW SOMETHING HERE
