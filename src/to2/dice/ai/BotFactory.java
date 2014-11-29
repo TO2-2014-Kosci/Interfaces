41493ccb6dc15d3f7e4e5e0c602401ea437da2a5
jlabuz <vaduz16@gmail.com>
2014-11-29 13:04:53 +0100

    interface IGameControllerFactory

--- src/to2/dice/ai/BotFactory.java
+++
@@ -1,8 +0,0 @@
-package to2.dice.ai;
-
-import to2.dice.game.BotLevel;
-import to2.dice.game.GameType;
-
-public interface BotFactory {
-    public Bot createBot(BotLevel level, GameType gameType, int maxTime);
-}
